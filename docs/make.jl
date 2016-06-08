8aa68e752c1c8d96b10a6423bbaa562207864a0e
Zachary Sunberg <sunbergzach@gmail.com>
2016-06-07 20:22:33 -0700

    added mdx_math dependency for building documentation html

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,7 @@ using Documenter, MCTS
 makedocs()
 
 deploydocs(
+    deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/JuliaPOMDP/MCTS.jl.git",
     julia = "release",
     osname = "linux"
