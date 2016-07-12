6d4f855a17e19c9b3e92a956ce3c1d3f2ed8fefc
Zachary Sunberg <sunbergzach@gmail.com>
2016-07-11 18:13:44 -0700

    got rid of incorrect comment in notebook

--- src/action_gen.jl
+++ src/action_gen.jl
@@ -1,3 +1,6 @@
+"""
+Generate a new action when the set of actions is widened.
+"""
 function next_action{S,A}(gen::RandomActionGenerator, mdp::Union{POMDP,MDP}, s, snode::DPWStateNode{S,A})
     if isnull(gen.action_space) 
         gen.action_space = Nullable{AbstractSpace}(actions(mdp))
