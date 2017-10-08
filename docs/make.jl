7a326c408da38b9ce922a3701c8a7ace3fcb089e
Zachary Sunberg <sunbergzach@gmail.com>
2017-10-07 23:04:05 -0700

    tried to deploy docs on julia 0.6

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,6 @@ makedocs()
 deploydocs(
     deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/JuliaPOMDP/MCTS.jl.git",
-    julia = "0.5",
+    julia = "0.6",
     osname = "linux"
 )
