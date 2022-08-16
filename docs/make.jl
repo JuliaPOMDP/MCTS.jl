81b2b9c9f58bea5dbc1cca7d3f1a4807792f66cb
Dylan Asmar <asmar@stanford.edu>
2022-08-16 00:03:55 -0700

    doc build update

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,18 @@
 using Documenter, MCTS
 
-makedocs()
+makedocs(
+    modules = [MCTS],
+    format = Documenter.HTML(),
+    sitename = "MCTS.jl",
+    pages = [
+        "index.md",
+        "vanilla.md",
+        "dpw.md",
+        "belief_mcts.md",
+    ]
+)
 
 deploydocs(
-    deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/JuliaPOMDP/MCTS.jl.git",
     versions = ["stable" => "v^", "v#.#"],
 )
