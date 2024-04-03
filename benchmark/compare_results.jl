5d0e44c8e2a45a2294943f96722e818956526256
Jan Mrkos <mrkosjan@gmail.com>
2024-04-03 15:29:11 +0200

    Setup base commit for benchmark comparison

--- benchmark/compare_results.jl
+++ benchmark/compare_results.jl
@@ -1,10 +1,10 @@
 using BenchmarkTools
 
 RES_DIR = "./benchmark/results"
-NEW_RESULTS = "benchres_removing_type_instabilities-38969fb6_2024-03-28_17-25-38.json"
+OLD_RESULTS = "benchres_benchmarking-8e44d82c_2024-04-03_15-03-48.json"
 
 # Load latest result
-OLD_RESULTS =  sort(readdir(RES_DIR), by=x->last(x,24)) |> last
+NEW_RESULTS =  sort(readdir(RES_DIR), by=x->last(x,24)) |> last
 
 old_results = BenchmarkTools.load(joinpath(RES_DIR, OLD_RESULTS))[1]
 new_results = BenchmarkTools.load(joinpath(RES_DIR, NEW_RESULTS))[1]
