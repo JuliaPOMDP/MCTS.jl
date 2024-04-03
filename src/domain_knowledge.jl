942aaf9747fd4b1054482cb5aa633fad766156d3
Jan Mrkos <mrkosjan@gmail.com>
2024-04-03 16:14:32 +0200

    Removed further abstract types from struct definitions

--- src/domain_knowledge.jl
+++ src/domain_knowledge.jl
@@ -19,12 +19,12 @@ Fields:
         If this is a Solver, solve(solver, mdp) will be called to find the rollout policy
         If this is a Policy, the policy will be used for rollouts
         If this is a Function, a POMDPToolbox.FunctionPolicy with this function will be used for rollouts
-    max_depth::Int
+    max_depth::Int (default 50)
         Rollout depth. If this is -1, it will roll out to the `depth` argument of the `MCTSSolver` from the root node.
-    eps::Float64
+    eps::Float64 (default 0.0)
         A small number; if γᵗ where γ is the discount factor and t is the time step becomes smaller than this, the rollout will be terminated.
 """
-mutable struct RolloutEstimator
+struct RolloutEstimator
     solver::Union{Solver,Policy,Function} # rollout policy or solver
     max_depth::Int
     eps::Float64
