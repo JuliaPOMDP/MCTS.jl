b43e3b8f03587398c010ea7190235e5ba9ec1672
Jan Mrkos <mrkosjan@gmail.com>
2024-04-03 16:07:47 +0200

    added more benchmark comparionsons

--- benchmark/compare_results.jl
+++ benchmark/compare_results.jl
@@ -1,12 +1,19 @@
 using BenchmarkTools
 
 RES_DIR = "./benchmark/results"
-OLD_RESULTS = "benchres_benchmarking-8e44d82c_2024-04-03_15-03-48.json"
+# OLD_RESULTS = "benchres_benchmarking-b9859ef4_2024-04-03_15-18-25.json"
+BASELINE_RESULTS = "benchres_benchmarking-8e44d82c_2024-04-03_15-03-48.json"
 
 # Load latest result
-NEW_RESULTS =  sort(readdir(RES_DIR), by=x->last(x,24)) |> last
+NEW_RESULTS =  sort(readdir(RES_DIR), by=x->last(x,24))[end]
+PREVIOUS_RESULTS = sort(readdir(RES_DIR), by=x->last(x,24))[end-1]
 
-old_results = BenchmarkTools.load(joinpath(RES_DIR, OLD_RESULTS))[1]
+baseline_results = BenchmarkTools.load(joinpath(RES_DIR, BASELINE_RESULTS))[1]
+previous_results = BenchmarkTools.load(joinpath(RES_DIR, PREVIOUS_RESULTS))[1]
 new_results = BenchmarkTools.load(joinpath(RES_DIR, NEW_RESULTS))[1]
 
-BenchmarkTools.judge(median(old_results), median(new_results))
+println("Comparing latest results to baseline: \n\t$NEW_RESULTS\n\tto\n\t$BASELINE_RESULTS:")
+@show BenchmarkTools.judge(median(baseline_results), median(new_results))
+
+println("\nComparing latest results to previous: \n\t$NEW_RESULTS\n\tto\n\t$PREVIOUS_RESULTS:")
+BenchmarkTools.judge(median(previous_results), median(new_results))
