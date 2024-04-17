f6a815db026d21c144346c8b9e5c5df3548f0f61
Jan Mrkos <mrkosjan@gmail.com>
2024-04-17 09:16:08 +0200

    Fixed order of arguments in result comparison

--- benchmark/compare_results.jl
+++ benchmark/compare_results.jl
@@ -13,7 +13,7 @@ end
 
 RES_DIR = "./benchmark/results"
 # OLD_RESULTS = "benchres_benchmarking-b9859ef4_2024-04-03_15-18-25.json"
-BASELINE_RESULTS = "benchres_benchmarking-8e44d82c_2024-04-03_15-03-48.json"
+BASELINE_RESULTS = "benchres_benchmarking-92ea2725_2024-04-16_16-42-56.json"
 
 # Load latest result
 NEW_RESULTS =  sort(readdir(RES_DIR), by=x->last(x,24))[end]
@@ -24,9 +24,9 @@ previous_results = BenchmarkTools.load(joinpath(RES_DIR, PREVIOUS_RESULTS))[1]
 new_results = BenchmarkTools.load(joinpath(RES_DIR, NEW_RESULTS))[1]
 
 println("Comparing latest results to baseline: \n\t$NEW_RESULTS\n\tto\n\t$BASELINE_RESULTS:")
-judgement =  BenchmarkTools.judge(median(baseline_results), median(new_results))
+judgement =  BenchmarkTools.judge(median(new_results), median(baseline_results))
 print_judgement(judgement)
 
 println("\nComparing latest results to previous: \n\t$NEW_RESULTS\n\tto\n\t$PREVIOUS_RESULTS:")
-judgement = BenchmarkTools.judge(median(previous_results), median(new_results))
+judgement = BenchmarkTools.judge(median(new_results), median(previous_results))
 print_judgement(judgement);
